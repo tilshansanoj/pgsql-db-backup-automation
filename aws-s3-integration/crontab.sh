b96eabee8c89fc48b819b446901f13af18a598d9
mahasona123 <tilshansanoj@gmail.com>
2025-05-28 23:12:19 +0530

    Fix output redirection in crontab.sh to ensure error logs are captured correctly

--- aws-s3-integration/crontab.sh
+++ aws-s3-integration/crontab.sh
@@ -1,1 +1,1 @@
-0 2 * * * /path/to/pg_backup_to-s3.sh  -u username -h db.hostname  -a -b s3_bucket_name -p password >> ~/backup_logs/pg_backup_$(date +\%Y\%m\%d).log 2>&
+0 2 * * * /path/to/pg_backup_to-s3.sh  -u username -h db.hostname  -a -b s3_bucket_name -p password >> ~/backup_logs/pg_backup_$(date +\%Y\%m\%d).log 2>&1
